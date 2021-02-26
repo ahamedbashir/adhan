97e1559460e62b6d4d074a462e6a74885e478625
Bash <ahamedb1991@gmail.com>
2021-02-26 13:04:20 -0500

    updated Threading for daily alert to limit to single thread

--- Adhan.py
+++ Adhan.py
@@ -1,5 +1,5 @@
 from datetime import date, datetime, timedelta
-from os import name
+from os import name, waitpid
 from threading import Event, Thread, Timer, main_thread
 import json
 import time
@@ -11,10 +11,11 @@ class Adhan(Thread):
     def __init__(self, event):
         Thread.__init__(self)
         self.stopped = event
+        self.PrayerName = ['Fajr',	'Dhuhr',	'Asr',	'Maghrib',	'Isha']
         self.currentPrayerName = ""
         self.currentPrayerTime = ""
-        self.nexttPrayerName = ""
-        self.nexttPrayerTime = ""
+        self.nextPrayerName = ""
+        self.nextPrayerTime = ""
 
     def start(self):
         stopFlag = Event()
@@ -25,30 +26,32 @@ class Adhan(Thread):
 
     def run(self):
         waitTime = 0
-        firsRun = True
+        firstRun = True
         now = datetime.now()
         nextDay = now + timedelta(days=1)
         nextDay = nextDay.replace(hour=1, minute=0, second=0, microsecond=0)
         waitTime2 = (nextDay - now).total_seconds()
         finalWaitTime = (timedelta(days=1)).total_seconds()
+
         while not self.stopped.wait(waitTime):
             today = datetime.now()
             print("Start Time", today)
-            print("Next day", nextDay)
-            waitTime = waitTime2 if firsRun else finalWaitTime
-            firsRun = False
-            self.scheduAdhan()
+            waitTime = waitTime2 if firstRun else finalWaitTime
+            waitTime = 30
+            firstRun = False
+            self.scheduleAdhan()
+            print("Next day will start")
 
     def playAdhan(self, prayerTime, prayerName):
         adhan = AudioPlayer("./Adhans/AdhanFajr.mp3") if prayerName == "Fajr" \
             else AudioPlayer("./Adhans/Adhan.mp3")
         print(prayerName, prayerTime)
-        adhan.play(block=True)
+        adhan.play(block=False)
         time.sleep(10)
         adhan.stop()
         adhan.close()
 
-    def scheduAdhan(self):
+    def scheduleAdhan(self):
         date = datetime.now()
         with open(f"./prayerTimes/{date.strftime('%B') }.json") as f:
             monthlyPrayerTimes = json.load(f)
@@ -56,20 +59,26 @@ class Adhan(Thread):
             # print(monthlyPrayerTimes[str(date.day)])
 
     def playDailyPrayerAlert(self, PrayerTimes):
-        for nextPrayerName in ('Fajr',	'Dhuhr',	'Asr',	'Maghrib',	'Isha'):
-            nextPrayerTime = PrayerTimes[nextPrayerName]
-            nextPrayerTime = datetime.strptime(
-                nextPrayerTime, '%H:%M:%S').time()
+        i = 0
+        while i < 5 and (waitTime := self.calcWaitTime(PrayerTimes[self.PrayerName[i]], self.PrayerName[i])) and not self.stopped.wait(waitTime):
+            if waitTime >= 0:
+                print("Time for ", self.PrayerName[i])
+                self.playAdhan(
+                    PrayerTimes[self.PrayerName[i]], self.PrayerName[i])
+            i += 1
 
-            timeNow = datetime.today()
-            delta_t = datetime.combine(
-                timeNow.today(), nextPrayerTime) - timeNow
-
-            secs = delta_t.total_seconds()
-
-            if secs > 0:
-                Timer(secs, self.playAdhan, [
-                    nextPrayerTime, nextPrayerName]).start()
+    def calcWaitTime(self, nextTimeStr, nextPrayerName):
+        nextPrayerTime = datetime.strptime(
+            nextTimeStr, '%H:%M:%S').time()
+        timeNow = datetime.today()
+        delta_t = datetime.combine(
+            timeNow.today(), nextPrayerTime) - timeNow
+        waitTime = delta_t.total_seconds()
+        waitTime = 60
+        if waitTime >= 0:
+            print("Time Now : ", timeNow.time())
+            print("Next Prayer Time : ", nextPrayerName, nextPrayerTime)
+        return waitTime
 
 
 if __name__ == "__main__":
